daa18b85ade5a37dd7ebf8c030f28b0e3f6bcb8b
Katelyn <katelyn.rimkunas@gmail.com>
2018-11-27 15:36:06 -0700

    controller & serializer

---
+++ app/controllers/api/v1/customer_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::CustomerController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+end
---
+++ spec/controllers/api/v1/customer_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe CustomerController, type: :controller do
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #show" do
+    it "returns http success" do
+      skip
+      get :show
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
