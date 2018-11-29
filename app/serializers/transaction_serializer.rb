00a7a306196828c76e7d5e752336601b71aeb1c9
Katelyn <katelyn.rimkunas@gmail.com>
2018-11-29 11:53:14 -0700

    transaction json updated

--- app/serializers/transaction_serializer.rb
+++ app/serializers/transaction_serializer.rb
@@ -2,14 +2,6 @@ class TransactionSerializer
 
   include FastJsonapi::ObjectSerializer
 
-  attributes :id, :credit_card_number, :credit_card_expiration_date, :result
-
-  # belongs_to :invoice
-
-  # TODO test this
-  attribute :invoice_id do |object|
-    object.invoices.pluck(:id)
-  end
-
+  attributes :id, :credit_card_number, :credit_card_expiration_date, :result, :invoice_id
 
 end
