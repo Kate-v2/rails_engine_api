b0a803cd9ae2dfec1f80fbecb0d4af4851d13d00
Katelyn <katelyn.rimkunas@gmail.com>
2018-11-28 22:39:50 -0700

    invoice tests functional and refactored with apihelper

--- spec/api/v1/invoices_request_spec.rb
+++ spec/api/v1/invoices_request_spec.rb
@@ -3,32 +3,38 @@ require "api_helper"
 
 
 RSpec.describe 'InvoicesAPI' do
+  include APIHelper
 
   before(:each) do
-    @qty = 2
-    @inv1, @inv2 = create_list(:invoice, @qty)
+    @customer = create(:customer)
+    @merchant = create(:merchant)
+    # @item_qty
+    # @item1, @item2 = create_list(:item, @item_qty, merchant: @merchant)
+
+    @inv1 = create(:invoice, merchant: @merchant, customer: @customer)
+    @inv2 = create(:invoice, merchant: @merchant, customer: @customer)
+    @qty = Invoice.count
   end
 
   describe 'INDEX' do
 
     before(:each) do
       get api_v1_invoices_path
-      # @invoices = JSON.parse(response.body)['data']
       @invoices = json_data
-      @invoice  = @invoices.first['attributes']
+      @invoice  = @invoices.first
     end
 
     it "Sends a List of Invoices" do
       expect(response).to be_successful
       expect(@invoices.count).to eq(@qty)
-      expect(@invoice['id']).to  eq(@inv1.id)
-      inv2 = @invoices.last['attributes']
-      expect(inv2['id']).to eq(@inv2.id)
+      expect( json_attribute(@invoice, 'id') ).to eq(@inv1.id)
+      invoice2 = @invoices.last
+      expect( json_attribute(invoice2, 'id') ).to eq(@inv2.id)
     end
 
     describe 'Invoice Public Attributes' do
-      it "ID"     { expect(@invoice['id']).to     eq(@inv1.id) }
-      it "Status" { expect(@invoice['status']).to eq(@inv1.status) }
+      it { json_attribute(@invoice, 'id').should     eq(@inv1.id)}
+      it { json_attribute(@invoice, 'status').should eq(@inv1.status)}
     end
   end
 
@@ -36,19 +42,18 @@ RSpec.describe 'InvoicesAPI' do
 
     before(:each) do
       get api_v1_invoice_path(@inv1)
-      # @invoice = JSON.parse(response.body)['data']
       @invoice = json_data
     end
 
     it "Sends a Specific Invoice" do
       expect(response).to be_successful
       expect(@invoice.class).to eq(Hash)
-      expect(@invoice['attributes']['id']).to eq(@inv1.id)
+      expect( json_attribute(@invoice, 'id') ).to eq(@inv1.id)
     end
 
     describe "Invoice Public Data" do
-      it "ID"     { expect(@invoice['attributes']['id']).to     eq(@inv1.id) }
-      it "Status" { expect(@invoice['attributes']['status']).to eq(@inv1.status) }
+      it { json_attribute(@invoice, 'id').should     eq(@inv1.id)}
+      it { json_attribute(@invoice, 'status').should eq(@inv1.status)}
     end
   end
 
