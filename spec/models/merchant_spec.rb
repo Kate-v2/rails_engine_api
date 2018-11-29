897d6b4c2fbe7f5a55f50d4ca7d0c06fd4636c69
Katelyn <katelyn.rimkunas@gmail.com>
2018-11-29 15:57:56 -0700

    validate_presence_of for merchant

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Merchant, type: :model do
   it { should have_many(:transactions).through(:invoices) }
   it { should have_many(:customers).through(:invoices) }
 
-
+  it { should validate_presence_of(:name) }
 
 
 end
