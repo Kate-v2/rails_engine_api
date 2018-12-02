3bd51a50d370b0051299464dc4b41b29456cd9aa
Katelyn <katelyn.rimkunas@gmail.com>
2018-12-02 16:56:41 -0700

    methods for customer params

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,28 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+
+  # --- Filter ---
+
+  def self.id_is(id)
+    find(id)
+  end
+
+  def self.created_on(date)
+    where(created_at: date)
+  end
+
+  def self.updated_on(date)
+    where(updated_at: date)
+  end
+
+  # --- Random ---
+
+  def self.random
+    order("RANDOM()").first
+  end
+
+
+
+
 end
