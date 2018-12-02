90f64649a47eea4163bbf5999581bd8484c3e549
Katelyn <katelyn.rimkunas@gmail.com>
2018-12-02 15:35:57 -0700

    remove unused code

--- app/controllers/api/v1/invoices/items_controller.rb
+++ app/controllers/api/v1/invoices/items_controller.rb
@@ -1,8 +1,6 @@
 class Api::V1::Invoices::ItemsController < ApplicationController
 
   def index
-    # binding.pry
-    # items = InvoiceItem.where(invoice_id: params[:invoice_id])
     items = Invoice.find(params[:invoice_id]).items
     render json: ItemSerializer.new(items)
   end
